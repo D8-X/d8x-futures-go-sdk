35f893c1854db4ee3022db1623f2a5d3071749d6
b1674927 <b1674927@protonmail.com>
2024-10-29 11:32:21 -0400

    test

--- pkg/d8x_futures/trade_test.go
+++ pkg/d8x_futures/trade_test.go
@@ -33,13 +33,13 @@ func TestSdkExec(t *testing.T) {
 		t.FailNow()
 	}
 	//err := sdk.New([]string{pk}, "42161") //arbitrum
-	err := sdk.New([]string{pk}, "421614") //arbitrum sepolia
+	//err := sdk.New([]string{pk}, "421614") //arbitrum sepolia
 	//err := sdk.New([]string{pk}, "195") //x-layer testnet
 	//err := sdk.New([]string{pk}, "196") //x-layer
 	//err := sdk.New([]string{pk}, "2442") //cardona
 	//err := sdk.New([]string{pk}, "1442") //zkevm testnet
-	//err := sdk.New([]string{pk}, "80084") //bartio
-	perp := "PENDLE-USD-STUSD"
+	err := sdk.New([]string{pk}, "80084") //bartio
+	perp := "DIRAC-HONEY-USDC"
 	if err != nil {
 		t.Log(err.Error())
 		t.FailNow()
